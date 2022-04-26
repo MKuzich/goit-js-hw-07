eceeadf9ab7b31a02f854224f41913c2c52eae06
Mykhailo Kuzich <timmkedth@gmail.com>
2022-04-26 21:32:22 +0300

    finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createImagesListMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImagesListMarkup(items) {
+  return items.map(({preview, original, description}) => {
+return `
+<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>
+`})
+.join("")
+}
+
+const links = galleryContainer.querySelectorAll('.gallery__link');
+
+links.forEach(el => el.addEventListener('click', e => e.preventDefault()));
+
+galleryContainer.addEventListener('click', onImageClick)
+
+function onImageClick(event) {
+    if (event.target.nodeName === "IMG") {
+        const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600">
+    `,)
+    instance.show();
+    document.addEventListener('keydown', event => { if (event.key === "Escape") {instance.close()}})
+    }
+    event.stopPropagation();
+}
+
+
+
+
+
+
